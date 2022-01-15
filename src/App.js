37063492af485c72a5ef7da4b0740c0be8ea9cd2
MoayadSalameh99 <75863926+MoayadSalameh99@users.noreply.github.com>
2022-01-15 22:16:23 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Teacher from './components/Teacher';
+import Admin from './components/Admin';
+import Activity from './components/Activity';
+import Student from './components/Student';
+
+function App() {
+	return (
+		/* test */
+		<div className='App'>
+			<Teacher />
+			<Admin />
+			<Activity />
+			<Student />
+		</div>
+	);
+}
+
+export default App;
